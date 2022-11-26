ce37c03bb050713e7bac24ac2d22654c2c83f563
Omar Aviles <oaviles@live.com>
2022-11-26 08:10:33 -0600

    Delete assign-policy.sh

--- policy/assign-policy.sh
+++
@@ -1,4 +0,0 @@
-
-az login --service-principal -u $1 -p $2 --tenant $3
-
-az policy assignment create --policy {Audit Tags for All Services} -p "{ \"Tag Name\": { \"value\": [ \"Env\" ] }, \"Tag Value\": { \"value\": [ \"Dev\" ] } }"
